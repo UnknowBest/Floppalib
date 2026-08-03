d3e6bc61b272154e265659b11a2d1aad42af424e
UnknowBest <103254965+UnknowBest@users.noreply.github.com>
2026-08-03 05:08:46 -0500

    Update released.lua

--- released.lua
+++ released.lua
@@ -1367,8 +1367,8 @@ local function getObjGen()
             Gui.Logo.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
             Gui.Logo.BackgroundTransparency = 1.000
             Gui.Logo.LayoutOrder = -5
-            Gui.Logo.Size = UDim2.new(1, 0, 0.100000001, 0)
-            Gui.Logo.ZIndex = 260
+            Gui.Logo.Size = UDim2.new(1, 0, 0.25, 0)
+            Gui.Logo.ZIndex = 122
             Gui.Logo.Image = "rbxassetid://95976497861614"
             Gui.Logo.ImageColor3 = Color3.fromRGB(134, 142, 255)
             Gui.Logo.ScaleType = Enum.ScaleType.Fit
